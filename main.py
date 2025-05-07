4b81b3d3077b29e413810d6b4499e3b78213e89f
Raphael Ndonga <nkibi53@gmail.com>
2025-05-07 14:18:11 +0300

    added boto ec2 setup

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import boto3
 import json
 
 def lambda_handler(event, context):
+    ec2_client = boto3.client("ec2")
+    response = ec2_client.describe_instances()
     return {
         "statusCode": 200,
-        "body": json.dumps("Hello from lambda piped x3")
+        "body": json.dumps(f'instances: {response}')
     }
