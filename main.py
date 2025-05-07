11b9a44c41774ed43532c918ae901ad6f42489f6
Raphael Ndonga <nkibi53@gmail.com>
2025-05-07 15:16:11 +0300

    added checking of instance state

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import json
 def lambda_handler(event, context):
     ec2_client = boto3.client("ec2")
     response = ec2_client.describe_instances()
+    instance_states=[]
+    for reservation in response["reservation"]:
+        for instance in reservation["Instances"]:
+            instance_states.append(instance['State']['Name'] == 'running')
     return {
         "statusCode": 200,
-        "body": json.dumps(f'instances: {response}')
+        "body": json.dumps(f'instances: {instance_states}')
     }
