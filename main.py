4d0f0e2c3a5c73f0f6274bf3655641fe50d26835
Raphael Ndonga <nkibi53@gmail.com>
2025-05-07 16:37:13 +0300

    condition for maxuptime to be 30 minutes before sending notification

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import datetime
 import boto3
 import json
 
+sns_client = boto3.client("sns")
+TOPIC_ARN="arn:aws:sns:eu-west-1:266735808339:ec2-uptime-alert"
+SUBJECT="[WARNING]: Excessive usage of EC2 Instance" 
+ec2_client = boto3.client("ec2")
+MAX_UPTIME_MINS=30
+
 def lambda_handler(event, context):
-    sns_client = boto3.client("sns")
-    ec2_client = boto3.client("ec2")
     response = ec2_client.describe_instances()
-    instance_states=[]
     for reservation in response["Reservations"]:
         for instance in reservation["Instances"]:
             instance_id = instance["InstanceId"]
@@ -17,15 +20,14 @@ def lambda_handler(event, context):
             print(instance_state)
             uptime = datetime.datetime.now(tz=launch_time.tzinfo) - launch_time
             uptime_minutes = uptime.total_seconds()/60
-
-            message=f'You cannot afford to have instance {instance_id} running any longer. It\'s been up for {uptime_minutes} minutes'
-            subject="[WARNING]: Excessive usage of EC2 Instance" 
-            sns_client.publish(
-                TopicArn="arn:aws:sns:eu-west-1:266735808339:ec2-uptime-alert",
-                Message= message,
-                Subject=subject
-            )
+            if float(uptime_minutes) > MAX_UPTIME_MINS:
+                message=f'You cannot afford to have instance {instance_id} running any longer. It\'s been up for {uptime_minutes} minutes'
+                sns_client.publish(
+                    TopicArn=TOPIC_ARN,
+                    Message= message,
+                    Subject=SUBJECT
+                )
     return {
         "statusCode": 200,
-        "body": json.dumps(f'instances: {instance_states}')
+        "body": json.dumps(f'instances')
     }
