17b517f352380b13ed9a986ec4949277c37c6666
Raphael Ndonga <nkibi53@gmail.com>
2025-05-07 15:20:59 +0300

    removed error

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     ec2_client = boto3.client("ec2")
     response = ec2_client.describe_instances()
     instance_states=[]
-    for reservation in response["reservation"]:
+    for reservation in response["Reservations"]:
         for instance in reservation["Instances"]:
             instance_states.append(instance['State']['Name'] == 'running')
     return {
