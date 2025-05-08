879a6cbaea2526709cf8aa33d7c60819c66254c5
Raphael Ndonga <nkibi53@gmail.com>
2025-05-08 10:52:42 +0300

    added required parameters in scheduler

--- trigger_eventscheduler.py
+++ trigger_eventscheduler.py
@@ -5,8 +5,14 @@ scheduler_client = boto3.client("scheduler")
 SCHEDULE_NAME = "ec2-uptime-cron"
 
 def lambda_handler(event, context):
+    current = scheduler_client.get_schedule(
+        Name=SCHEDULE_NAME
+    )
     response = scheduler_client.update_schedule(
         Name=SCHEDULE_NAME,
+        FlexibleTimeWindow=current["FlexibleTimeWindow"],
+        ScheduleExpression=current["ScheduleExpression"],
+        Target=current["Target"],
         State="ENABLED"
     )
     return {
