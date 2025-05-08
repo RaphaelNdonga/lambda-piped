d4646058269099b321898d16802eef98112a8395
Raphael Ndonga <nkibi53@gmail.com>
2025-05-08 12:23:28 +0300

    add detail

--- trigger_eventscheduler.py
+++ trigger_eventscheduler.py
@@ -13,7 +13,7 @@ def lambda_handler(event, context):
         FlexibleTimeWindow=current["FlexibleTimeWindow"],
         ScheduleExpression=current["ScheduleExpression"],
         Target=current["Target"],
-        State=event.scheduled_state
+        State=event.detail.scheduled_state
     )
     return {
         "statusCode": 200,
